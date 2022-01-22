72fa9736e65988c55f656757f53f04e0bf7c3f90
Lzadhito <Lzadhito@gmail.com>
2022-01-22 10:06:26 +0700

    feat(home): initial setup -> ts, emotion, theme-ui, babel, delete unused template codes

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import { css } from "@emotion/react";
+
+const Home: NextPage = () => {
+  return (
+    <div
+      css={css`
+        background-color: blue;
+      `}
+    >
+      soldier 76
+    </div>
+  );
+};
+
+export default Home;
